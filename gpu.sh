38c87d5c612414138eed7bd3386620fc02b99d40
nyxnix <nyxcyrix@gmail.com>
2025-02-11 19:39:31 -0500

    Fixed cut not grabbing usage, thanks nvidia

--- gpu.sh
+++ gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-USAGE=$(nvidia-smi | grep % | cut -c 70-73 | sed -e 's/^\s*//' -e '/^$/d')
+USAGE=$(nvidia-smi | grep '%' | awk '{print $13}' | sed 's/%//g')
 TEMP=$(nvidia-settings -query GPUCoreTemp -t)
 echo "GPU: $USAGE% $TEMP°C"
